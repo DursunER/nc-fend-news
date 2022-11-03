26b3c550ca97da092d3ab20be3a2a984043543b3
DursunER <105427734+DursunER@users.noreply.github.com>
2022-11-03 10:31:40 +0000

    task5

--- src/components/TopicArticles.jsx
+++ src/components/TopicArticles.jsx
@@ -1,33 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function TopicArticles({currentArticles, isLoading }) {
+function TopicArticles({ currentArticles, isLoading }) {
+  const [visable, setVisable] = useState(false);
 
- 
-    const [hide, setHide] = useState(true);
-
-    if (isLoading) return <p>Loading...</p>
-    else return (
+  if (isLoading) return <p>Loading...</p>;
+  else
+    return (
       <div>
         <h2> Topic Article list</h2>
-      <div >
-            {currentArticles.map(({article_id, title, topic, author, body, created_at, votes, comment_count}) => {
-                return (
-                  <div key={article_id} >
-                      <button type="button" className="collapsible" onClick={() => setHide(false)}><strong>Title:</strong>  {title}</button>
-                          <div className={hide ? "hiden" : null}>
-                                <p><strong>Topic:</strong>: {topic}</p>
-                                <p><strong>Author:</strong> {author}</p>
-                                <p><strong>Content:</strong> {body}</p>
-                                <p><strong>Created on:</strong>{created_at}</p>
-                                <p><strong>Votes:</strong> {votes}</p>
-                                <p><strong>Comment count:</strong> {comment_count}</p> 
-                            </div>
-                        </div>
-                        )
-                    })}
+        <div>
+          {currentArticles.map(
+            ({
+              article_id,
+              title,
+              topic,
+              author,
+              body,
+              created_at,
+              votes,
+              comment_count,
+            }) => {
+              return (
+                <div key={article_id}>
+                  <button
+                    type="button"
+                    className="collapsible"
+                    onClick={() => setVisable(!visable)}
+                  >
+                    <strong>Title:</strong> {title}
+                  </button>
+                  <div
+                    className={visable ? "element-visable" : "element-hidden"}
+                  >
+                    <p>
+                      <strong>Topic:</strong>: {topic}
+                    </p>
+                    <p>
+                      <strong>Author:</strong> {author}
+                    </p>
+                    <p>
+                      <strong>Content:</strong> {body}
+                    </p>
+                    <p>
+                      <strong>Created on:</strong>
+                      {created_at}
+                    </p>
+                    <p>
+                      <strong>Votes:</strong> {votes}
+                    </p>
+                    <p>
+                      <strong>Comment count:</strong> {comment_count}
+                    </p>
+                  </div>
+                </div>
+              );
+            }
+          )}
+        </div>
       </div>
-    </div>
-  )
+    );
 }
 
-export default TopicArticles
+export default TopicArticles;
